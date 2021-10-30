bbddfd90f70159d76a1fa29faad38064393a160b
edRibeiro <eder.sonz@hotmail.com>
2021-10-30 09:55:46 -0300

    refactor: unifying all routes

--- src/app.ts
+++ src/app.ts
@@ -8,7 +8,7 @@ const configureExpress = async () => {
   app.use(express.json());
   app.use(express.urlencoded({ extended: false }));
   app.use(cors());
-  app.use('/', routes);
+  app.use('/api/v1/', routes);
 
   return app;
 };
--- src/routes.ts
+++ src/routes.ts
@@ -1,12 +1,16 @@
 import * as express from 'express';
-import feedbacksRoute from './modules/feedbacks/feedback.routes';
+import feedbackRoutes from './modules/feedbacks/feedback.routes';
+import userRoutes from './modules/users/user.routes';
+import authRoutes from './modules/users/user.routes';
 
 const router = express.Router();
-
-router.use('/feedbacks', feedbacksRoute);
 
 router.all('/', (req, res) => {
   return res.status(200).json({ msg: 'Services are up!' });
 });
+
+router.use('/users', userRoutes);
+router.use('/feedbacks', feedbackRoutes);
+router.use('/auth', authRoutes);
 
 export default router;
