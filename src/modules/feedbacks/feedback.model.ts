861617b0c1fa5f49e7f737e148a94a795d2c3311
edRibeiro <eder.sonz@hotmail.com>
2021-10-30 09:49:22 -0300

    feat: adding routes to module feedbacks.

---
+++ src/modules/feedbacks/feedback.model.ts
@@ -0,0 +1,32 @@
+import mongoose, { Schema, Document } from 'mongoose';
+import { IUser } from '../users/user.model';
+
+export interface IFeedback extends Document {
+  createdAt: Date,
+  finalfeedback: string;
+  improvementpoints: string;
+  maintainpoints: string;
+  owner: IUser['_id'];
+  suggestions: string;
+  user: IUser['_id'];
+
+}
+
+export var  FeedbackSchema: Schema = new Schema({
+  owner: { type: Schema.Types.ObjectId, required: true },
+  user: { type: Schema.Types.ObjectId, required: true },
+  finalfeedback: { type: String, required: true },
+  improvementpoints: { type: String, required: true },
+  maintainpoints: { type: String, required: true },
+  suggestions: { type: String, required: true },
+});
+
+FeedbackSchema.pre("save", function(next) {
+  let now = new Date();
+  if (!this.createdAt) {
+    this.createdAt = now;
+  }
+  next();
+});
+
+export default mongoose.model<IFeedback>('Feedback', FeedbackSchema);
